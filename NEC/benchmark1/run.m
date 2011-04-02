8dce71951a1fc6d85ec70b91fa4bedc92fb79606
philippe.dewagter <philippe.dewagter@4c301745-1aca-d447-c1c3-5c556e32b406>
2011-04-02 22:58:15 +0000

    Fit search space

--- NEC/benchmark1/run.m
+++ NEC/benchmark1/run.m
@@ -1,28 +1,71 @@
+%% Benchmark#1.
+
 clc; clear all; close all;
 display(['Benchmark #1']);
 
-% Define search space.
+%% Define search space.
 X = 0:0.5:90;   % Theta (degree)
 Y = 0.5:0.02:3; % Length (meter)
 Z = [];         % Directivity (dimensionless)
 
-% Compute directivity.
+%% Compute directivity (NEC).
 % (expected time: 36 seconds)
 tic
 for i = 1:length(Y),
     display(['./CALLNEC.exe ' num2str(i)]);
-    system(['./CALLNEC.exe ' num2str(i)]);
+    system(['./outputs/CALLNEC.exe ' num2str(i)]);
     z = dlmread('GAIN.OUT');
+    system(['rm NEC.INP NEC.OUT GAIN.OUT']);
     Z = [Z 10.^(z(1:181,2)/10)];
 end
+Z = Z';
 toc
 
-% Display results.
+%% Plot directivity (NEC).
+figure('Name', 'Benchmark#1 - (NEC)');
+
 [X, Y] = meshgrid(X, Y);
-surf(X, Y, Z', 'EdgeAlpha', 0.3);
+surf(X, Y, Z, 'EdgeAlpha', 0.3);
+
 xlabel('Theta (degree)', 'FontSize', 20);
 ylabel('Length (meter)', 'FontSize', 20);
 zlabel('Directivity (dimensionless)', 'FontSize', 20);
 
-% Save results to disk.
-dlmwrite('directivity-b1.txt', Z);
+%% Save directivity (NEC).
+dlmwrite('outputs/directivity-b1.txt', Z');
+save('outputs/directivity-b1.m', 'X', 'Y', 'Z');
+
+%% Compute fitted directivity (cubic interpolation).
+opts = fitoptions('cubicinterp');
+opts.Weights = zeros(1,0);
+opts.Normalize = 'on';
+[fitresult, gof] = fit([X(:), Y(:)], Z(:), 'cubicinterp', opts);
+
+%% Plot fitted directivity (Cubic Interpolation).
+figure('Name', 'Benchmark#1 - (NEC + Cubic Interpolation)');
+
+plot(fitresult, [X(:), Y(:)], Z(:));
+xlabel('Theta (degree)', 'FontSize', 20);
+ylabel('Length (meter)', 'FontSize', 20);
+zlabel('Directivity (dimensionless)', 'FontSize', 20);
+view(-53.5, 40);
+
+%% Plot fitted directivity (Cubic Interpolation).
+figure('Name', 'Benchmark#1 - (NEC + Cubic Interpolation)');
+
+step = 0.0001;
+XX = 0:step:90;
+YY = 0.5:step:3;
+
+[XX, YY] = meshgrid(XX, YY);
+ZZ = fitresult(XX(:), YY(:));
+ZZ = reshape(ZZ, size(XX));
+
+surf(XX, YY, ZZ, 'EdgeAlpha', 0.3);
+xlabel('Theta (degree)', 'FontSize', 20);
+ylabel('Length (meter)', 'FontSize', 20);
+zlabel('Directivity (dimensionless)', 'FontSize', 20);
+
+%% Save directivity (Cubic Interpolation).
+dlmwrite('outputs/directivity-b1-ci.txt', ZZ');
+save('outputs/directivity-b1-ci.m', 'XX', 'YY', 'ZZ');
